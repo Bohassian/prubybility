4132bf92ded7e4ec883e6d1e3d5f75a969d70769
Jacob Lee <jacob@unabridgedsoftware.com>
2021-03-14 14:47:53 -0500

    Continuing to troubleshoot makefile

--- extconf.rb
+++ extconf.rb
@@ -1,5 +1,5 @@
 abort "Rust compiler required (https://www.rust-lang.org/)" if `which rustc`.empty?
 
 require 'mkmf'
-create_makefile '/'
-system("rake build")
+create_makefile 'prubybility-rust'
+File.write('Makefile', "all:\n\tcargo rustc --release\nclean:\n\trm -rf ./lib/prubybility/libprubybility.so\ninstall: mv -f ./target/release/libprubybility.so ./lib/prubybility;")
