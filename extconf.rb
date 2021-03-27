a21f9081342d0c856e4375b83a311558a0bed222
Jacob Lee <jacob@unabridgedsoftware.com>
2021-03-27 12:29:36 -0500

    Fix gem install

--- extconf.rb
+++ extconf.rb
@@ -2,4 +2,4 @@ abort "Rust compiler required (https://www.rust-lang.org/)" if `which rustc`.emp
 
 require 'mkmf'
 create_makefile 'prubybility-rust'
-File.write('Makefile', "all:\n\tcargo rustc --release\nclean:\n\trm -rf ./lib/prubybility/libprubybility.so\ninstall: mv -f ./target/release/libprubybility.so ./lib/prubybility;")
+`rake rust_build`
